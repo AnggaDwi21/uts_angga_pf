78f13a742a07a430fb808a93786f0de7b4a4f03f
Wahyudi110402 <93042039+AnggaDwi21@users.noreply.github.com>
2022-04-18 21:17:16 +0800

    fiirs commit

---
+++ app/Views/dashboard.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/admin_navbar'); ?>
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Hello</h1>
+            <p>Untuk melihat crud nya klik pada Navbar</p>
+
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
